55fd24170bd369cfc5dabb3adeab620841162329
Xavier <isiomastewart@gmail.com>
2022-02-04 01:28:46 +0100

    21 Savage - padlock

--- chain/utils/merkletree.go
+++ chain/utils/merkletree.go
@@ -1,4 +1,9 @@
+package "utils"
 
+import (
+  "fmt",
+  "crypto/sha256"
+)
 
 type MerkleTree interface {
   
@@ -6,4 +11,15 @@ type MerkleTree interface {
 
 func MerkleRoot () {
   if()
+}
+
+func hashLeaves(leaves []Transaction) []string {
+  hashBook := [leaves.len]
+  for _,v := range leaves {
+    hash := sha256.New()
+    hash.Write([]byte(fmt.Sprintf("%v", leaves)))
+    
+    return fmt.Sprintf("%x", h.Sum(nil))
+    
+  }
 }
