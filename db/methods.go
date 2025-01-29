9add305409888340b24ab6906a47d63aebf8fbab
Alviss Raghnall <isiomastewart@gmail.com>
2025-01-29 23:53:56 +0100

    db functionality

--- db/methods.go
+++ db/methods.go
@@ -5,34 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"blockchain/types"
+	"strconv"
 )
-
-// Transaction represents a blockchain transaction
-type Transaction struct {
-	ID       int64              `json:"id"`
-	Version  int32              `json:"version"`
-	Locktime uint32             `json:"locktime"`
-	Inputs   []TransactionInput  `json:"inputs"`
-	Outputs  []TransactionOutput `json:"outputs"`
-}
-
-// TransactionInput represents a transaction input with script
-type TransactionInput struct {
-	ID             int64   `json:"id"`
-	PreviousTxHash string  `json:"previous_tx_hash"`
-	OutputIndex    int64   `json:"output_index"`
-	ScriptSig      []byte  `json:"script_sig"`
-	Sequence       uint32  `json:"sequence"`
-}
-
-// TransactionOutput represents a transaction output with script
-type TransactionOutput struct {
-	ID           int64   `json:"id"`
-	Value        float64 `json:"value"`
-	ScriptPubKey []byte  `json:"script_pubkey"`
-	ScriptType   string  `json:"script_type"`
-	Address      string  `json:"address,omitempty"`
-}
 
 // AddBlock stores a new block in the database
 func (bdb *BlockchainDB) AddBlock(block *types.Block) error {
@@ -168,7 +142,7 @@ func (bdb *BlockchainDB) GetTransaction(txID int64) (*types.Transaction, error) 
 
 	return tx, nil
 }
-
+/**
 // GetBlock retrieves a block and all its transactions by hash
 func (bdb *BlockchainDB) GetBlock(hash string) (*types.Block, error) {
 	var block types.Block
@@ -212,4 +186,120 @@ func (bdb *BlockchainDB) GetBlock(hash string) (*types.Block, error) {
 	}
 
 	return &block, nil
+}
+*/
+
+func (bdb *BlockchainDB) GetBlock(hash string) (*types.Block, error) {
+    var block types.Block
+
+    // Construct query that joins blocks, transactions, inputs and outputs
+    query := `
+        SELECT
+            b.index, b.timestamp, b.prev_hash, b.hash, b.nonce,
+            b.miner, b.blocksize, b.difficulty,
+            t.id as tx_id, t.version, t.locktime,
+            ti.id as input_id, ti.previous_tx_hash, ti.output_index,
+            ti.script_sig, ti.sequence,
+            to.id as output_id, to.value, to.script_pubkey,
+            to.script_type, to.address
+        FROM blocks b
+        LEFT JOIN transactions t ON t.block_hash = b.hash
+        LEFT JOIN transaction_inputs ti ON ti.transaction_id = t.id
+        LEFT JOIN transaction_outputs to ON to.transaction_id = t.id
+        WHERE b.hash = ?
+        ORDER BY t.id, ti.id, to.id`
+
+    rows, err := bdb.db.Query(query, hash)
+    if err != nil {
+        return nil, fmt.Errorf("failed to query block data: %v", err)
+    }
+    defer rows.Close()
+
+    // Maps to keep track of transactions we've already seen
+    txMap := make(map[int64]*types.Transaction)
+    var currentTxID int64
+    var tx *types.Transaction
+
+    // Scan the first row to get block data
+    first := true
+    for rows.Next() {
+        var (
+            txID, inputID, outputID                               sql.NullInt64
+            version, locktime                                     sql.NullInt64
+            prevTxHash, scriptSig, scriptType, scriptPubKey, address           sql.NullString
+            outputIndex, sequence                                sql.NullString
+            value                                                sql.NullFloat64
+        )
+
+        err := rows.Scan(
+            &block.Index, &block.Timestamp, &block.PrevHash, &block.Hash,
+            &block.Nonce, &block.Miner, &block.BlockSize, &block.Difficulty,
+            &txID, &version, &locktime,
+            &inputID, &prevTxHash, &outputIndex, &scriptSig, &sequence,
+            &outputID, &value, &scriptPubKey, &scriptType, &address,
+        )
+        if err != nil {
+            return nil, fmt.Errorf("failed to scan row: %v", err)
+        }
+
+        // Skip block initialization after first row
+        if first {
+            first = false
+        }
+
+        // If this row contains transaction data
+        if txID.Valid {
+            // If this is a new transaction
+            if txID.Int64 != currentTxID {
+                currentTxID = txID.Int64
+                tx = &types.Transaction{
+                    ID:       txID.Int64,
+                    Version:  int32(version.Int64),
+                    Locktime: uint32(locktime.Int64),
+                }
+                txMap[currentTxID] = tx
+                block.Transactions = append(block.Transactions, *tx)
+            }
+
+            // Add input if present
+            if inputID.Valid {
+                // Convert outputIndex and sequence from string to uint64 and uint32
+                outputIndexUint, err := strconv.ParseUint(outputIndex.String, 10, 64)
+                if err != nil {
+                    return nil, fmt.Errorf("failed to parse outputIndex: %v", err)
+                }
+                sequenceUint, err := strconv.ParseUint(sequence.String, 10, 32)
+                if err != nil {
+                    return nil, fmt.Errorf("failed to parse sequence: %v", err)
+                }
+
+                input := types.Input{
+                    ID:             inputID.Int64,
+                    PreviousTxHash: []byte(prevTxHash.String),
+                    OutputIndex:    outputIndexUint,
+                    ScriptSig:      []byte(scriptSig.String),
+                    Sequence:       uint32(sequenceUint),
+                }
+                tx.Inputs = append(tx.Inputs, input)
+            }
+
+            // Add output if present
+            if outputID.Valid {
+                output := types.Output{
+                    ID:           uint64(outputID.Int64), // Convert int64 to uint64
+                    Amount:       value.Float64,
+                    ScriptPubKey: []byte(scriptPubKey.String),
+                    ScriptType:   scriptType.String,
+                    Address:      []byte(address.String),
+                }
+                tx.Outputs = append(tx.Outputs, output)
+            }
+        }
+    }
+
+    if first {
+        return nil, fmt.Errorf("block not found")
+    }
+
+    return &block, nil
 }
